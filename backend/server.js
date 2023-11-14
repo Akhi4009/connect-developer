6d07929ad817503510fb78850d948bab63f9a847
Akhilesh yadav <akhileshyadav310596@gmail.com>
2023-11-14 21:04:25 +0530

    connect to server and database

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,15 @@
 const express=require('express')
+const connection=require("./db")
 const app=express()
 app.get("/",(req,res)=>res.send('API running'))
 const PORT=process.env.PORT || 5000
 
-app.listen(PORT,()=> console.log(`Server started on port ${PORT}`))
+app.listen(PORT,async()=>{
+try{
+    await connection;
+    console.log("connected to db")
+}catch(err){
+    console.log({"msg":"not connected to database","error":err.message})
+}
+ console.log(`Server started on port ${PORT}`)
+})
