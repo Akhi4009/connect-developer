de7fc8ab59ceee599ee832e66ee5487ab23d0a5a
Akhilesh yadav <akhileshyadav310596@gmail.com>
2023-11-15 20:02:01 +0530

    user schema

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,19 @@
 const express=require('express')
 const connection=require("./db")
+const userRouter=require("./routes/users")
+const authRouter=require("./routes/auth")
+const postRouter=require("./routes/posts")
+const profileRouter=require("./routes/profile")
 const app=express()
+
 app.get("/",(req,res)=>res.send('API running'))
 const PORT=process.env.PORT || 5000
+
+//Define Routes
+app.use("/users",userRouter)
+app.use("/auth",authRouter)
+app.use("/post",postRouter)
+app.use("/profile",profileRouter)
 
 app.listen(PORT,async()=>{
 try{
