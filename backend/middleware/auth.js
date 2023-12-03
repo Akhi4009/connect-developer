5a1358d2752f7f146a779ddca413689b5b4982fd
Akhilesh yadav <akhileshyadav310596@gmail.com>
2023-12-04 00:51:04 +0530

    middleware

---
+++ backend/middleware/auth.js
@@ -0,0 +1,23 @@
+const jwt = require("jsonwebtoken")
+
+require('dotenv').config()
+
+module.exports =async (req,res,next)=>{
+
+    // Get token from header
+    const token = req.header('x-auth-token')
+    if(!token){
+        return res.status(401).json({msg:"No token, authorization denied"})
+    }
+
+    // Verify token
+    try{
+        const decoded=jwt.verify(token,process.env.secretkey)
+
+        req.user=decoded.user
+        next()
+    }catch(err){
+        res.status(401).json({msg: 'Token is not valid'})
+
+    }
+}
