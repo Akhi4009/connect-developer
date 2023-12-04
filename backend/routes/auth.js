d515e0841b7a1c568b2925ae40c6e3819c6af72d
Akhilesh yadav <akhileshyadav310596@gmail.com>
2023-12-04 09:44:34 +0530

    login compleet

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,5 +1,7 @@
 const express=require("express");
 const router=express.Router();
+const bcrypt=require("bcryptjs")
+const jwt=require("jsonwebtoken")
 const User=require("../models/user")
 const auth = require("../middleware/auth")
 
@@ -13,6 +15,52 @@ router.get("/", auth ,async(req,res)=>{
         res.status(500).send('Server Error')
 
     }
+})
+
+router.post("/login",async(req,res)=>{
+
+    const { email, password} = req.body
+    try{
+    // See if user exists 
+    
+    let user = await User.findOne({email}).select('+password')
+    //  console.log(user)
+    
+
+    if(!user){
+       return  res.status(400).json({errors:[{msg: 'Invalid Credential' }] })
+    }
+
+    const isMatch= await bcrypt.compare(password,user.password)
+
+    if(!isMatch){
+        return res.status(400).json({errors:[{msg: 'Invalid Credential' }] })
+    }
+
+   // Return jsonwebtoken
+
+   const payload= {
+    user:{
+        id:user.id
+    }
+   };
+
+
+
+ let token=  jwt.sign(payload,
+    process.env.secretkey,
+    {expiresIn:360000})
+   console.log(token)
+  res.json({
+    msg:"Resitered sucessfully",
+    token
+  })
+
+}catch(err){
+
+    console.error(err.message)
+    res.status(500).send('Server error')
+}
 })
 
 module.exports=router
